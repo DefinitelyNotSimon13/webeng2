f599e3dbd0a396ed8921da7f4e2ed57dff87b4b4
Simon Blum <simon21.blum@gmail.com>
2025-11-11 10:43:21 +0100

    Fix css import

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import { useGeolocation } from "../hooks/useGeoLocation.js";
-import "../css/map.css";
+import "../css/Map.css";
 
 function AutoLocateOnce({ pos, zoom }) {
   const map = useMap();
