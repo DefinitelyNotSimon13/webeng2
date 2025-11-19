ee46add21f4504da75cd1928d3bc86665bbb9b85
DosKobold <hanspeter56@t-online.de>
2025-11-19 14:22:46 +0100

    feat: add content to about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,28 +1,33 @@
 import React from "react";
-import { Page, Navbar, Block, BlockTitle } from "framework7-react";
+import { Page, Navbar, Block, BlockTitle, Link } from "framework7-react";
 
 const AboutPage = () => (
   <Page>
     <Navbar title="About" backLink="Back" />
-    <BlockTitle>About My App</BlockTitle>
+    <BlockTitle>About our site</BlockTitle>
     <Block>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni
-        molestiae laudantium dignissimos est nobis delectus nemo ea alias
-        voluptatum architecto, amet similique, saepe iste consectetur in
-        repellat ut minus quibusdam!
+        Hi, we are a group of CS students from the DHBW Ravensburg and developed
+        this small site as part of our web engineering course. You can set a
+        position on the map and see nearby points of interests. You can get more
+        information about them from wikipedia. There is also the possibility to
+        access a point by longitude and latitude. The first point should be the
+        position of your device.
       </p>
+      <Link href="https://github.com/DefinitelyNotSimon13/webeng2" external>
+        Github repository
+      </Link>
+    </Block>
+
+    <BlockTitle>Contact</BlockTitle>
+    <Block>
       <p>
-        Molestias et distinctio porro nesciunt ratione similique, magni
-        doloribus, rerum nobis, aliquam quae reiciendis quasi modi. Nam a
-        recusandae, fugiat in ea voluptates fuga eius, velit corrupti
-        reprehenderit dignissimos consequatur!
-      </p>
-      <p>
-        Blanditiis, cumque quo adipisci. Molestiae, dolores dolorum quos
-        doloremque ipsa ullam eligendi commodi deserunt doloribus inventore
-        magni? Ea mollitia veniam nostrum nihil, iusto doloribus a at! Ea
-        molestiae ullam delectus!
+        Simon Blum &lt;simon21.blum@gmail.com&gt; <br />
+        Ben Oeckl &lt;ben@oeckl.com&gt; <br />
+        Maximilian Reiner Rodler &lt;maximilian.reiner.rodler@gmail.com&gt;{" "}
+        <br />
+        Marius Maurer &lt;marius.maurer@freenet.de&gt; <br />
+        Paul Stöckler &lt;paul.stoeckle@t-online.de&gt; <br />
       </p>
     </Block>
   </Page>
