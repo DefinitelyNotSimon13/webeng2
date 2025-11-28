2e9bc305a99f431b04e54307e417c8039ecb1ae6
benoeckl <ben@oeckl.com>
2025-11-28 11:48:13 +0100

    feat: improve markers for current/target positions

--- src/components/map/TargetLocationMarker.jsx
+++ src/components/map/TargetLocationMarker.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { useContext } from "react";
 import LocationContext from "../../js/context";
 import { Marker, Popup } from "react-leaflet";
+import { createMarkerIcon } from "./customMarkerIcon";
+
+const targetLocationIcon = createMarkerIcon("#e53e3e");
 
 export default function TargetLocationMarker() {
   const { targetLocation } = useContext(LocationContext);
   return (
     <>
       {targetLocation && (
-        <Marker position={[targetLocation.lat, targetLocation.lng]}>
+        <Marker
+          position={[targetLocation.lat, targetLocation.lng]}
+          icon={targetLocationIcon}
+        >
           <Popup>
             Target Position:
             {targetLocation.lat.toFixed(5)}, {targetLocation.lng.toFixed(5)}
